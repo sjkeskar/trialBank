69a4504d8cdaf2e9bb6da954dd83b81951540072
sjkeskar <sjkeskar@gmail.com>
2022-07-15 10:56:13 +0530

    emi calculator added

---
+++ app.js
@@ -0,0 +1,46 @@
+ints = { "home": 7, "car": 9, "personal": 12 }
+var contactNum = (e) => {
+    e.preventDefault()
+    x = e.target.value
+    console.log(x)
+    return true
+}
+
+const netLogin = (e) => {
+    console.log(e.target.value);
+}
+
+const roiAmount = () => {
+    var x = document.getElementById("typeloan").value
+    if (x !== "")
+        document.getElementById("roi").innerHTML = ints[document.getElementById("typeloan").value] + "%"
+    else
+        document.getElementById("roi").innerHTML = ""
+}
+
+const calculateEMI = () => {
+    interestRate = ints[document.getElementById("typeloan").value]
+    principal = document.getElementById("amount").value
+    duration = document.getElementById("duration").value * 12
+    applicantName = document.getElementById("applicant").value
+    flag = true
+    if (duration === 0 || duration > 25 || duration <= 1) {
+        flag = false
+        alert("Please choose correct duration")
+    }
+    if (interestRate === 0) {
+        flag = false
+        alert("Please choose your loan type")
+    }
+    if (principal === 0) {
+        flag = false
+        alert("Please Enter correct loan amount")
+    }
+    if (flag) {
+        roi = (interestRate / 12) / 100
+        stub = Math.pow((1 + roi), duration)
+        emi = principal * roi * (stub / (stub - 1))
+        console.log(emi, stub, roi, interestRate);
+        document.getElementById("res").innerHTML = `<p> ${applicantName}'s EMI for ${duration} months at ${interestRate}% interest Rate is ${emi}</p>`
+    }
+}
